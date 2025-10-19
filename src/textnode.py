39ed92a37659b05134de370120b8a9ef6887bed6
Anish Surendra <anish.surendra@twidpay.com>
2025-10-19 16:18:11 +0530

    text nodes to html

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     PLAIN_TEXT = "plain"
@@ -24,3 +25,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+        if text_node.text_type == TextType.PLAIN_TEXT:
+            return LeafNode(None, text_node.text)
+        elif text_node.text_type == TextType.BOLD_TEXT:
+            return LeafNode("b", text_node.text)
+        elif text_node.text_type == TextType.ITALIC_TEXT:
+            return LeafNode("i", text_node.text)
+        elif text_node.text_type == TextType.CODE_TEXT:
+            return LeafNode("code", text_node.text)
+        elif text_node.text_type == TextType.LINK_TEXT:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        elif text_node.text_type == TextType.IMAGE_TEXT:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        else:
+            raise ValueError("Unknown TextType")
