03b05c427d59e81b5dbb1a31cf49ebd9e3973399
Anish Surendra <anish.surendra@twidpay.com>
2025-10-19 23:43:34 +0530

    same as bootdev

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,9 +4,9 @@ class HTMLNode:
         self.value = value
         self.children = children
         self.props = props
-    
+
     def to_html(self):
-        raise NotImplementedError("")
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
         if self.props is None:
@@ -18,7 +18,8 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
-    
+
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
@@ -32,17 +33,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
-    
+
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag=tag, value=None, children=children, props=props)
-    
+        super().__init__(tag, None, children, props)
+
     def to_html(self):
-        if not self.tag:
-            raise ValueError("Parent nodes must have a tag to convert to HTML")
-        if not self.children:
-            raise ValueError("Parent nodes must have children to convert to HTML")
-        children_html = ''.join([child.to_html() for child in self.children])
-        return f'<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>'
-        
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,30 +2,44 @@ import unittest
 
 from textnode import TextNode, TextType, text_node_to_html_node
 
+
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", TextType.ITALIC_TEXT, url="https://example.com")
-        expected_repr = "TextNode(This is a text node, italic, https://example.com)"
-        self.assertEqual(repr(node), expected_repr)
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
-    def test_url(self):
-        node = TextNode("This is a text node", TextType.LINK_TEXT, url="https://example.com")
-        self.assertEqual(node.url, "https://example.com")
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
-        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_image(self):
-        node = TextNode("This is an image", TextType.IMAGE_TEXT, "https://www.boot.dev")
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
@@ -35,10 +49,11 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         )
 
     def test_bold(self):
-        node = TextNode("This is bold", TextType.BOLD_TEXT)
+        node = TextNode("This is bold", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
+
 
 if __name__ == "__main__":
     unittest.main()
