e3625ce922be69c5d8ffbba2711a1e4961d8419f
Anish Surendra <anish.surendra@twidpay.com>
2025-10-20 15:22:55 +0530

    copying from static to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from textnode import TextNode, TextType
+import os
+import shutil
 
-
+def copy_folder(static, public):
+    if os.path.exists(public):
+        shutil.rmtree(public)
+    os.makedirs(public)
+    dirs = os.listdir(static)
+    for d in dirs:
+        if os.path.isfile(os.path.join(static, d)):
+            shutil.copy(os.path.join(static, d), os.path.join(public, d))
+        else:
+            copy_folder(os.path.join(static, d), os.path.join(public, d))
+            
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    copy_folder("static", "public")
     print(node)
 
 
